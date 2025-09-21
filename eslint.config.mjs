53205336e3cf3c8a504791c24400f7e62a4e4171
Cauã Drefahl <cauadrefahl@gmail.com>
2025-09-21 16:36:39 -0300

    feat: configure Prettier with ESLint and VSCode settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,22 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 120,
+          singleQuote: true,
+          trailingComma: 'all',
+          tabWidth: 2,
+          semi: true,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+        },
+      ],
     },
   },
 );
